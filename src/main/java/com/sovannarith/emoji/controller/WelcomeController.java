8f40cc696bc3c5564408016bd97040475c2b703f
Cheav Sovannarith <cheavsovannarith@gmail.com>
2019-11-25 17:28:26 +0700

    add emoji library

--- src/main/java/com/sovannarith/emoji/controller/WelcomeController.java
+++ src/main/java/com/sovannarith/emoji/controller/WelcomeController.java
@@ -1,7 +1,12 @@
 package com.sovannarith.emoji.controller;
 
+import com.vdurmont.emoji.Emoji;
+import com.vdurmont.emoji.EmojiManager;
+import com.vdurmont.emoji.EmojiParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
@@ -9,6 +14,18 @@ public class WelcomeController {
     @GetMapping({"/", "/index"})
     public String main() {
         return "index"; //view
+    }
+
+    @RequestMapping("/lele")
+    public @ResponseBody
+    String getEmoji(){
+        String str = "An :grinning:awesome :smiley:string &#128516;with a few :wink:emojis!";
+        StringBuilder result = new StringBuilder(EmojiParser.parseToUnicode(str));
+        for (Emoji emoji : EmojiManager.getAll()) {
+            result.append(emoji.getUnicode());
+        }
+        System.out.println(result);
+        return "\uD83D\uDE00" + result;
     }
 
 }
